e2a5f37759ad59b8eef0f8b4fb0770367afd0aab
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-01-12 12:39:21 -0300

    adição do prisma

---
+++ src/prisma/index.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient = PrismaClient()
+
+export default prismaClient;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
-import express, {Request, Response, NextFunction} from "express";
+import express, {Request, Response, NextFunction, response} from "express";
+import "express-async-errors";
 import { router } from "./routes";
 
 const app = express();
 const port = 3333;
 app.use(express.json());
 app.use(router);
+
+app.use((err: Error, req:Request, res: Response, next: NextFunction ) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            error: err.message,
+        });
+    }
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error.'
+    })
+})
 
 app.listen(port, () => {
     console.log("Servidor rodando na porta 3333 - Projeto Controle de Estoque")
