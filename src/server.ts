76c1c377a3f6a78bc5a9a4da5cfa61ecc128bbf3
Everton Brandão de Moura <evertonbrandao137@hotmail.com>
2024-01-19 11:19:36 -0300

    adição do cors e do swagger, junto ao inicio da documentação da API

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
 import express, {Request, Response, NextFunction, response} from "express";
 import "express-async-errors";
 import { router } from "./routes";
+import cors from "cors";
+import path from "path";
+import swaggerDocument from "../swagger.json"
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
 const port = 3333;
 app.use(express.json());
-app.use(router);
+app.use(cors());
+app.use("/v1",router);
+app.use("/api-docs",swaggerUi.serve,swaggerUi.setup(swaggerDocument));
+app.use("/files",express.static(path.resolve(__dirname, "..","tmp")));
 
 app.use((err: Error, req:Request, res: Response, next: NextFunction ) => {
     if (err instanceof Error) {
@@ -18,6 +25,12 @@ app.use((err: Error, req:Request, res: Response, next: NextFunction ) => {
         message: 'Internal server error.'
     })
 })
+
+app.get("/items", (req: Request,res: Response) => {
+    return res.json({
+        message: "Termos de serviço"
+    })
+});
 
 app.listen(port, () => {
     console.log("Servidor rodando na porta 3333 - Projeto Controle de Estoque")
